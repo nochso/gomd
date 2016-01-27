92ec751debb9a0c544e8d1f70bada36304f0655b
Marcel Voigt <mv@noch.so>
2016-01-27 17:56:13 +0100

    Proof of concept

---
+++ gomd.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+)
+
+type Configuration struct {
+	Port         string
+	DocumentRoot string
+	ShowHelp     bool
+}
+
+var args Configuration
+
+func main() {
+	//	wd, err := os.Getwd()
+	//	if err != nil {
+	//		wd = "."
+	//	}
+	flag.StringVar(&args.DocumentRoot, "d", ".", "Document root")
+	flag.StringVar(&args.Port, "p", "12345", "Listening port used by webserver")
+	flag.BoolVar(&args.ShowHelp, "h", false, "Show this help")
+	flag.Parse()
+
+	if args.ShowHelp {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/asset/", http.StripPrefix("/asset/", http.FileServer(http.Dir("asset"))))
+	mux.HandleFunc("/", RootHandler)
+
+	go WaitForServer(args.Port)
+	err := http.ListenAndServe(":"+args.Port, mux)
+	if err != nil {
+		log.Fatal("Failed to listen and serve: ", err)
+	}
+}
+
+func RootHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-type", "text/html")
+	io.WriteString(w, req.URL.String()+"<br>")
+	io.WriteString(w, req.RequestURI+"<br>")
+	io.WriteString(w, "hello, world!<br>")
+	files, _ := filepath.Glob("*.md")
+	for _, file := range files {
+		io.WriteString(w, "<a href=\"#\">"+file+"</a><br>")
+	}
+
+	//	cwd, _ := os.Getwd()
+	//	fis, _ := ioutil.ReadDir(cwd)
+	//	for _, fi := range fis {
+	//		io.WriteString(w, fi.Name()+"<br>")
+	//	}
+	io.WriteString(w, "<link rel=\"stylesheet\" href=\"asset/css/font-awesome.min.css\">")
+	io.WriteString(w, "<link rel=\"stylesheet\" href=\"asset/css/simplemde.min.css\"><script src=\"asset/js/simplemde.min.js\"></script>")
+	io.WriteString(w, "<textarea></textarea>")
+	io.WriteString(w, "<script>var simplemde = new SimpleMDE({autoDownloadFontAwesome: false, spellChecker: false });</script>")
+}
+
+func WaitForServer(port string) {
+	log.Print("Waiting for listener on port " + port)
+	url := "http://localhost:" + port
+	for {
+		time.Sleep(time.Millisecond * 50)
+		resp, err := http.Get(url)
+		if err != nil {
+			continue
+		}
+		resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			continue
+		}
+		break
+	}
+	log.Println("Opening " + url)
+	command := exec.Command("explorer", url)
+	_ = command.Run()
+}
